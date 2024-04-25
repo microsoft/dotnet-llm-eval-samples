e295c18e833ac3a5c3ec168b094d2b40dff13d20
Maho Pacheco <mictlan@gmail.com>
2024-04-25 17:37:04 +0000

    Fixing eval prompts, and removing incoherent tests

--- examples/userstorygenerator/UserStoryGenerator.Tests/EvalTests.cs
+++ examples/userstorygenerator/UserStoryGenerator.Tests/EvalTests.cs
@@ -20,10 +20,8 @@ public class EvalTests : IClassFixture<PromptEvalDotNetFixture>
 
     [Theory]
     [InlineData("CRM software", "Create a login page", "marketing")]
-    [InlineData("zoo", "Learn to swim", "duck")]
     [InlineData("IoT project", "IaC for base infrastructure", "developer")]
     [InlineData("IoT new product", "spike in the data processor in the edge", "software engineer")]
-    [InlineData("catering", "try beer in different bars", "connoisseur")]
     public async void Should_BeCoherentAndGrounded(string description, string projectContext, string personaName)
     {
         var kernel = _promptEvaluator.Kernel;
@@ -34,18 +32,21 @@ public class EvalTests : IClassFixture<PromptEvalDotNetFixture>
 
         var modelOutput = new ModelOutput()
         {
-            Input = $"Generate a user story for {personaName} so it can {description}",
+            Input = $"Generate a user story for a {personaName} persona, doing {description}",
             Output = $"{userStory!.Title} - {userStory!.Description}"
         };
+
+        _output.WriteLine($"Q: {modelOutput.Input}");
+        _output.WriteLine($"A: {modelOutput.Output}.");
         
         var coherence = await (new CoherenceEval(kernel)).Eval(modelOutput);
         var groundedness = await (new GroundednessEval(kernel)).Eval(modelOutput);
         var relevance = await (new RelevanceEval(kernel)).Eval(modelOutput);
-        
+
         Assert.Multiple(
-            () => Assert.True(coherence >= 3, $"Coherence of {userStory.Title} - score {coherence} [min 3]"),
-            () => Assert.True(groundedness >= 3, $"Groundedness of {userStory.Title} - score {groundedness} [min 3]"),
-            () => Assert.True(relevance >= 3, $"Relevance of {userStory.Title} - score {relevance} [min 3]")
+            () => Assert.True(coherence >= 3, $"Coherence of {userStory.Title} - score {coherence}, expecting min 3."),
+            () => Assert.True(groundedness >= 3, $"Groundedness of {userStory.Title} - score {groundedness}, expecting min 3."),
+            () => Assert.True(relevance >= 3, $"Relevance of {userStory.Title} - score {relevance}, expecting min 3.")
         );
     }
 }
